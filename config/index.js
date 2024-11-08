7f006f4866eeedfd790140f694d517ed63a203d7
kjvhu <17731766510@163.com>
2024-11-08 20:42:34 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   appid: "wxfdc0e3fee063ebf1",
   appsecret: "5aaaaf5da7c9d4956c8b7712cb5845a8",
   template_id: "iuIKTW2CoAiqCiHXFAHEiRqzRzRhHLqHkkWfPHSYk7o",
-  touser: "oAvOF6aCZPqoB3vLuW61XhmhYlGc",
+  touser: "oAvOF6aCZPqoB3vLuW61XhmhYlGc,oAvOF6aYCQr1aDMjPkRaKNdWIP4E",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
   city: "沧州",
